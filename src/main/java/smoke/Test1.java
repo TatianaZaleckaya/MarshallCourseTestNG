a32e3740c8d8f7c7e9ffcb96e4891ec4ba9b1e1b
Tatiana Zaleckya <nekhaevna2212@gmail.com>
2022-03-22 22:00:14 +0300

    parameters

--- src/main/java/smoke/Test1.java
+++ src/main/java/smoke/Test1.java
@@ -1,23 +1,23 @@
 package smoke;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
-
 public class Test1 {
+    @Parameters({"db"})
+    @Test(groups = {"smokeTest", "regress"})
+    public void test1(String db) {
+        System.out.println("smoke.Test1" + db);
+    }
 
-    @Test (groups = {"smokeTest", "regress"})
-    public void test1(){
+    @Test(groups = {"regress"})
+    public void test11() {
         System.out.println("smoke.Test1");
     }
 
-    @Test (groups = {"regress"})
-    public void test11(){
-        System.out.println("smoke.Test1");
-    }
-
-    @Test (groups = {"smokeTest"})
-    public void test111(){
+    @Test(groups = {"smokeTest"})
+    public void test111() {
         System.out.println("smoke.Test111");
     }
 }
