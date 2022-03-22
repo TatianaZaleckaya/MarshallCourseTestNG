9182a13aa285f20c3ef24026ddfdde5857c2a3a4
Tatiana Zaleckya <nekhaevna2212@gmail.com>
2022-03-22 22:18:25 +0300

    data provider

--- src/main/java/smoke/Test1.java
+++ src/main/java/smoke/Test1.java
@@ -1,11 +1,27 @@
 package smoke;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
 public class Test1 {
+
+    //возвращает массив массивов из объекта
+    @DataProvider(name = "db")
+    public static Object[][] dbData () {
+        return new Object[][]{
+                {"SQL", new Integer(1)},
+                {"NOSQL", new Integer(2)}
+        };
+    }
+
+
+
+
+
     @Parameters({"db"})
+    //параметеризированные тесты. прописываем в хмл
     @Test(groups = {"smokeTest", "regress"})
     public void test1(String db) {
         System.out.println("smoke.Test1" + db);
